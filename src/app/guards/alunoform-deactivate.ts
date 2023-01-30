5c8717352d447f1de9356c46d1b1ca783f189b35
g-oliveira7 <studying.online@outlook.com>
2023-01-30 11:18:14 -0300

    Generic canDeactivate and Confirmation dialog update

--- src/app/guards/alunoform-deactivate.ts
+++ src/app/guards/alunoform-deactivate.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 
-import { ConfirmationService } from './../components/confirmation/confirmation.service';
 import { IFormCanDeactivate } from './iform-candeactivate';
 
 @Injectable()
